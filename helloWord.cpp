f39f742f10a346afa79bad36ce9c17f424314e0f
Bruno Bartolomasi <brunobrt@hotmail.com>
2020-06-21 22:25:23 -0300

    my first cpp program

---
+++ helloWord.cpp
@@ -0,0 +1,9 @@
+// This program outputs "Hello, World!" to the monitor
+
+#include "D:/Codigos_Estudo/c++/Cpp_Intro_Studies/std_lib_facilities.h" 
+
+int main() //C++ programs start by executing the function main
+{
+    cout << "Hello, World!\n";  //output "Hello, World!"
+    return 0;
+}
