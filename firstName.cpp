c57b1125b1d0c05896ac9a7cb9a8f0d9eb6a42f2
Bruno Bartolomasi <brunobrt@hotmail.com>
2020-06-22 23:23:15 -0300

    segundo cap

---
+++ firstName.cpp
@@ -0,0 +1,9 @@
+#include "D:/Codigos_Estudo/c++/Cpp_Intro_Studies/std_lib_facilities.h" 
+
+int main()
+{
+    cout << "Please enteder your first name (followed by 'enter')\n";
+    string first_name;
+    cin >> first_name; // read characters into first_name
+    cout << "Hello, " << first_name << "!\n";
+}
