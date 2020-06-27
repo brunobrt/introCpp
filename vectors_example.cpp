d222a8e37bca72383fb36be1af02d0505caaf0d4
Bruno Bartolomasi <brunobrt@hotmail.com>
2020-06-27 00:39:16 -0300

    vectors

---
+++ vectors_example.cpp
@@ -0,0 +1,18 @@
+#include "D:/Codigos_Estudo/c++/Cpp_Intro_Studies/std_lib_facilities.h" 
+
+int main()
+{
+    vector<double> temps;           // temperatures
+    for (double temp; cin >> temp;) // read into temp
+        temps.push_back(temp);      // put temp into vector
+
+    // compute mean temperature
+    double sum = 0;
+    for (double x : temps) sum += x;
+    cout << "Average temperature: " << sum/temps.size() << '\n';
+
+    // compute the median temperature
+    sort(temps) ; 
+    cout << "Mediam temperature: " << temps[temps.size()/2] << '\n';
+
+}
